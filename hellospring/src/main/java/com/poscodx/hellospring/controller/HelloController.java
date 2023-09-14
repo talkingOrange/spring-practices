d3a00f301a87acb5f76c5fd13a413c779172f8ba
[talkingOrange] <sonrisa-bonita@naver.com>
2023-09-14 15:48:27 +0900

    [ feat ] spring basics

--- hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.poscodx.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -10,4 +13,41 @@ public class HelloController {
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
 	}
+	
+	@RequestMapping("/hello2")
+	public String hello2(String name) {
+		System.out.println(name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(String n) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		mav.addObject("name", n);
+		
+		return mav;
+	}
+
+	//이 방법 추천!!
+	@RequestMapping("/hello4")
+	public String hello4(String n, Model model) {
+		model.addAttribute("name",n);
+		
+		return "/WEB-INF/views/hello3.jsp";
+	}
+	
+	//////////////////////////////////////////
+	
+	@ResponseBody
+	@RequestMapping("/hello5")
+	public String hello5() {
+		return "안녕 스프링씨";
+	}
+	
+	@RequestMapping("/hello6")
+	public String hello6() {
+		return "redirect:/hello";
+	}
+
 }
