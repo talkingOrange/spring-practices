68566aba774dcfafbc17222fe68b60fba0172a82
[talkingOrange] <sonrisa-bonita@naver.com>
2023-09-15 13:38:09 +0900

    [ feat ] emaillist03 finish

--- emaillist03/src/main/java/com/poscodx/emaillist/repository/EmaillistRepository.java
+++ emaillist03/src/main/java/com/poscodx/emaillist/repository/EmaillistRepository.java
@@ -136,7 +136,7 @@ public class EmaillistRepository {
 		
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			String url = "jdbc:mariadb://192.168.0.172:3307/webdb?charset=utf8";
+			String url = "jdbc:mariadb://192.168.0.178:3307/webdb?charset=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
