790dde2cc39a832fd0ffc191dfa4c2e03367aff6
[talkingOrange] <sonrisa-bonita@naver.com>
2023-09-15 12:23:27 +0900

    [ feat ] spring

--- hellospring/src/main/java/com/poscodx/hellospring/controller/UserController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/UserController.java
@@ -2,6 +2,9 @@ package com.poscodx.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @RequestMapping 클래스 + 메서드 매핑
@@ -14,10 +17,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 
-	@RequestMapping("/joinform")
+	@RequestMapping(value="/join", method=RequestMethod.GET)
 	public String joinform() {
-		return "/WEB-INF/views/joinform.jsp";
-
+		return "/WEB-INF/views/join.jsp";
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo userVo) {
+		System.out.println("UserController.join(): UserDao.insert("+ userVo+")");
+		return "redirect:/";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update")	
+	public String update(@RequestParam("n") String name) {
+		/**
+		 * 만일 n이라는 이름의 URL 파라미터가 없으면
+		 * 400 bad request 에러가 난다. 
+		 */
+		return "UserController.update(" + name +")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update2")	
+	public String update2(@RequestParam(value="n", required=false, defaultValue="") String name) {
+		return "UserController.update(" + name +")";
 	}
 
+	@ResponseBody
+	@RequestMapping("/list")
+	public String list(@RequestParam(value="p", required=true, defaultValue="1") int pageNo) {
+		return "UserController.update(" + pageNo + ")";
+	}
+	
 }
