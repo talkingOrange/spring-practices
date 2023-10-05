5c4016f50e8db1e43847d868234b8b76c91ada53
[talkingOrange] <sonrisa-bonita@naver.com>
2023-10-05 14:03:52 +0900

    [ feat ] container finish

---
+++ container/src/main/java/com/poscodx/container/videosystem/mixing/VideoSystemConfig.java
@@ -0,0 +1,17 @@
+package com.poscodx.container.videosystem.mixing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ *	 <------------- JavaConfig1, JavaCongif2 
+ *		import 
+ *
+ * JavaConfig1 + JavaConfig2
+ * 
+ */
+@Configuration
+@Import({DVDConfig.class, DVDPlayerConfig.class})
+public class VideoSystemConfig {
+	
+}
