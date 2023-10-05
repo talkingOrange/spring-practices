78add3157fab38d0ff0278501c463d2b5634d71f
[talkingOrange] <sonrisa-bonita@naver.com>
2023-10-05 12:10:27 +0900

    [ feat ] xml config

--- container/src/main/java/com/poscodx/container/config/videosystem/DVDPlayerConfig.java
+++ container/src/main/java/com/poscodx/container/config/videosystem/DVDPlayerConfig.java
@@ -1,8 +1,44 @@
 package com.poscodx.container.config.videosystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.poscodx.container.videosystem.Avengers;
+import com.poscodx.container.videosystem.DVDPlayer;
+import com.poscodx.container.videosystem.DigitalVideoDisc;
 
 @Configuration
 public class DVDPlayerConfig {
+	@Bean
+	public DigitalVideoDisc avengers() {
+		return new Avengers();
+	}
+	
+	// 주입(Injection)하기 I
+	// Bean 생성메소드를 직접 호출하는 방법
+	// 생성자 주입
+	@Bean("dvdPlayer")
+	public DVDPlayer dvdPlayer01() {
+		return new DVDPlayer(avengers());
+	}
+	
+	// 주입(Injection)하기 II
+	// Parameter로 Bean을 전달하는 방법
+	// 생성자 주입
+	@Bean
+	public DVDPlayer dvdPlayer02(DigitalVideoDisc dvd) {
+		return new DVDPlayer(dvd);
+	}
 
+	// 주입(Injection)하기 III
+	// Parameter로 Bean을 전달하는 방법
+	// setter 주입
+	@Bean
+	public DVDPlayer dvdPlayer03(DigitalVideoDisc dvd) {
+		DVDPlayer dvdPlayer = new DVDPlayer();
+		dvdPlayer.setDvd(dvd);
+		
+		return dvdPlayer;
+	}
+	
 }
--- container/src/main/java/com/poscodx/container/videosystem/IronMan.java
+++ container/src/main/java/com/poscodx/container/videosystem/IronMan.java
@@ -1,11 +1,12 @@
 package com.poscodx.container.videosystem;
 
-public class Avengers implements DigitalVideoDisc {
-	private String title = "Avengers";
+public class IronMan implements DigitalVideoDisc {
+	private String title = "Iron Man";
 	private String studio = "MARVEL";
 	
 	@Override
 	public String play() {
 		return "Playing Movie " + studio + "'s " + title;
 	}
+
 }
